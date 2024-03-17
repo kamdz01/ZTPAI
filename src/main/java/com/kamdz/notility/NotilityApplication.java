61209a55b41e6d1b2a914eaa08174aa873f6086a
Kamil Dziedzic <kamdz01@gmail.com>
2024-03-17 13:08:51 +0100

    initial commit

---
+++ src/main/java/com/kamdz/notility/NotilityApplication.java
@@ -0,0 +1,13 @@
+package com.kamdz.notility;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotilityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotilityApplication.class, args);
+	}
+
+}
